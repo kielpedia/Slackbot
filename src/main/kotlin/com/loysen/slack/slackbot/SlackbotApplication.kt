974b6f15b5db0fefbdeb4cd037c0a84c603ffca4
Kiel Loysen <kloysen@grubhub.com>
2019-04-27 14:37:35 -0500

    ISSUE-2 - make external call to slack when a message is sent that matches our trigger

--- src/main/kotlin/com/loysen/slack/slackbot/SlackbotApplication.kt
+++ src/main/kotlin/com/loysen/slack/slackbot/SlackbotApplication.kt
@@ -4,7 +4,10 @@ import com.loysen.slack.slackbot.event.SlackProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
 
+@EnableAsync
 @SpringBootApplication
 @EnableConfigurationProperties(SlackProperties::class)
 class SlackbotApplication
--- src/main/kotlin/com/loysen/slack/slackbot/event/EventCallbackService.kt
+++ src/main/kotlin/com/loysen/slack/slackbot/event/EventCallbackService.kt
@@ -1,19 +1,41 @@
 package com.loysen.slack.slackbot.event
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+import java.net.URI
+
+
+private val logger = KotlinLogging.logger {}
 
 @Service
-class EventCallbackService @Autowired constructor(val slackProperties: SlackProperties){
+class EventCallbackService @Autowired constructor(val slackProperties: SlackProperties, val restTemplate: RestTemplate) {
 
-    fun handleCallback(event: SlackEvent?): EventResponse {
-        if ("message" == event?.type) {
-            val inputText = event.text ?: ""
-            if (slackProperties.messageTrigger.toRegex().containsMatchIn(inputText)) {
-
+    fun handleCallback(event: SlackEvent?) {
+        GlobalScope.launch {
+            if ("message" == event?.type) {
+                val inputText = event.text ?: ""
+                if (slackProperties.messageTrigger.toRegex().containsMatchIn(inputText)) {
+                    sendMessageToSlack(event)
+                }
             }
         }
+    }
 
-        return EventResponse(null)
+    private fun sendMessageToSlack(event: SlackEvent) {
+        val uri = URI(slackProperties.slackMessagePostUrl)
+
+        val response: ResponseEntity<Void> = restTemplate.postForEntity(
+                uri,
+                CreateMessage(token = slackProperties.messageToken, channel = event.channel, text = "Kotlin is fun"),
+                Void::class.java)
+
+        if (response.statusCode.isError) {
+            logger.error { "Unable to post message back to slack, statusCode=${response.statusCodeValue}" }
+        }
     }
 }
