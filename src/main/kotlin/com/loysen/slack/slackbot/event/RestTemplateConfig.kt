9a3629df221918c9ec3011cd5cb509654c4ea4d9
Kiel Loysen <kloysen@grubhub.com>
2019-04-27 20:34:11 -0500

    Change the profile of resttemplate config to only be disabled for test profile

--- src/main/kotlin/com/loysen/slack/slackbot/event/RestTemplateConfig.kt
+++ src/main/kotlin/com/loysen/slack/slackbot/event/RestTemplateConfig.kt
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.web.client.RestTemplate
 
-@Profile("production")
+@Profile("!test")
 @Configuration
 class RestTemplateConfig {
 
